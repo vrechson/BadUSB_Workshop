fee117516d5d4d74d3d3b60775f7832a68e7d59f
matheus vrech <whoismath@gmail.com>
2018-01-06 00:04:55 -0300

    Delete payliad-windows.ps1

--- payliad-windows.ps1
+++
@@ -1,6 +0,0 @@
-$client = New-Object System.Net.WebClient
-$client.DownloadFile("http://www.indiewire.com/wp-content/uploads/2017/07/rick-and-morty.png" , "rick-and-morty.png")
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\rick-and-morty.png"
-Set-ItemProperty -path 'HKCU:\Software\Microsoft\Internet Explorer\Desktop\General' -name wallpaper -value "%USERPROFILE%\rick-and-morty.png"
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
-exit
