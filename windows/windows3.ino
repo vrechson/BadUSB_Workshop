e8561c0e6b9e14373cd18ef03b23c7f3e5b8142b
matheus vrech <whoismath@gmail.com>
2018-01-29 03:19:01 -0200

    make it works in my vm

--- windows/windows3.ino
+++ windows/windows3.ino
@@ -6,8 +6,9 @@ void setup() {
 }
 
 void loop() {
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); /* Open execute dialog. */
-  delay(100); /* Delay until it opens. */
-  DigiKeyboard.println(F("iexplore -k http://fakeupdate.net/win10u/index.html")); /* Open powershell. */
+  DigiKeyboard.delay(100); /* Delay until it opens. */
+  DigiKeyboard.println(F("iexplore -k http://fakeupdate.net/win10u/index.html")); /* Open fake update. */
   for(;;) { /* run just once. */ }
 }
